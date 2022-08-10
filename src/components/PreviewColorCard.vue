3a01c9a4e5a38e8a769ed4f02181acf83bf4f8b2
LucineAnna <906243432@qq.com>
2022-08-10 12:38:24 +0800

    UF3无对应组件时展示的色块组件

---
+++ src/components/PreviewColorCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="color-preview">
+        <p class="color-p">{{previewTitle}}</p>
+        <div class="color-div" :style="{backgroundColor:previewColor}"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['previewTitle','previewColor'],
+}
+</script>
+
+<style>
+.color-preview {
+  display: flex;
+  justify-content: center;
+  height: 60px;
+  line-height: 60px;
+  margin:40px auto;
+  text-align: right;
+  color:black;
+  font-weight: 500;
+  font-size: 16px;
+  font-family: PingFangSC-Medium;
+}
+.color-p{
+  width: 20%;
+  margin-right: 30px;
+}
+.color-div{
+  height: 60px;
+  width: 50%;
+  border-radius: 3px;
+}
+</style>
