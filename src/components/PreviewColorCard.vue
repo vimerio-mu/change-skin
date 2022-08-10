d93a04bec8a12d433e251fa62c5a037f151abe24
LucineAnna <906243432@qq.com>
2022-08-10 14:32:19 +0800

    预览色块优化

--- src/components/PreviewColorCard.vue
+++ src/components/PreviewColorCard.vue
@@ -1,36 +1,52 @@
 <template>
     <div class="color-preview">
-        <p class="color-p">{{previewTitle}}</p>
-        <div class="color-div" :style="{backgroundColor:previewColor}"></div>
+        <div class="color-text">{{previewTitle}}</div>
+        <div class="color-div" :style="styleObject"></div>
     </div>
 </template>
 
 <script>
 export default {
     props: ['previewTitle','previewColor'],
+    computed: {
+      styleObject(){
+        let color = this.previewColor.toLowerCase()
+        if(color == 'white' || color == '#fff' || color=='#ffffff'){
+          color = 'lightGrey'
+        }
+        return {
+          backgroundColor:this.previewColor,
+          borderColor:color,
+        }
+      }
+    }
 }
 </script>
 
 <style>
 .color-preview {
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   height: 60px;
   line-height: 60px;
   margin:40px auto;
-  text-align: right;
   color:black;
   font-weight: 500;
   font-size: 16px;
   font-family: PingFangSC-Medium;
 }
-.color-p{
-  width: 20%;
-  margin-right: 30px;
+.color-text{
+  display: flex;
+  justify-content: flex-start;
+  width: 30%;
+  min-width: 230px;
+  /* margin-right: 30px; */
 }
 .color-div{
   height: 60px;
-  width: 50%;
+  width: 60%;
+  min-width: 40px;
   border-radius: 3px;
+  border: 1px solid;
 }
 </style>
